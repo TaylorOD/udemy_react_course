06c6c8e9219716487a9b63215b716a4fa3c9c36b
taylorod <taylorsunwantedemails@gmail.com>
2021-08-31 09:00:19 -0400

    added state to app

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,76 +1,120 @@
 "use strict";
 
-// visibility toggle text
-// Button - when clicked shows something and changes to/from hide text to show text
-// Button shows text or removes text when clicked
-// text can be anything like hey this is visible
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-// Look at app.js and counterExample.js
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 // src/playground/build-it-visibility.js
 // babel src/playground/build-it-visibility.js --out-file=public/scripts/app.js --presets=env,react --watch
 
-var app = {
-  title: "Visibility Toggle",
-  someText: "",
-  buttonText: "Show Text"
+// VisibilityToggle -> render, constuctor, handleToggle
 
-  // let visibility = false
+// make visbility toggle and set to false
 
-};var appRoot = document.getElementById("app");
+var VisibilityToggle = function (_React$Component) {
+  _inherits(VisibilityToggle, _React$Component);
 
-var toggleText = function toggleText() {
-  if (app.someText.length <= 0) {
-    app.someText = "Here is some text!";
-  } else {
-    app.someText = "";
+  function VisibilityToggle(props) {
+    _classCallCheck(this, VisibilityToggle);
+
+    var _this = _possibleConstructorReturn(this, (VisibilityToggle.__proto__ || Object.getPrototypeOf(VisibilityToggle)).call(this, props));
+
+    _this.handleVisibility = _this.handleVisibility.bind(_this);
+    _this.state = {
+      visibility: false
+    };
+    return _this;
   }
-  toggleButtonText();
-  visibilityToggle();
-  visibility = !visibility;
-};
-var toggleButtonText = function toggleButtonText() {
-  if (app.buttonText === "Show Text") {
-    app.buttonText = "Hide Text";
-  } else {
-    app.buttonText = "Show Text";
-  }
-};
 
-var visibilityToggle = function visibilityToggle() {
-  var visibilityHTML = React.createElement(
-    "div",
-    null,
-    React.createElement(
-      "h1",
-      null,
-      app.title
-    ),
-    React.createElement(
-      "button",
-      { onClick: toggleText },
-      app.buttonText,
-      visibility ? "Hide Text" : "Show Text"
-    ),
-    app.someText && React.createElement(
-      "p",
-      null,
-      app.someText
-    ),
-    visibility && React.createElement(
-      "div",
-      null,
-      React.createElement(
-        "p",
+  _createClass(VisibilityToggle, [{
+    key: "handleVisibility",
+    value: function handleVisibility() {
+      this.setState(function (prevState) {
+        return {
+          visibility: !prevState.visibility
+        };
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
         null,
-        " Here is some text! "
-      )
-    )
-  );
-  ReactDOM.render(visibilityHTML, appRoot);
-};
+        React.createElement(
+          "h1",
+          null,
+          "Visibility Toggle"
+        ),
+        React.createElement(
+          "button",
+          { onClick: this.handleVisibility },
+          this.state.visibility ? "Hide Text" : "Show Text"
+        ),
+        this.state.visibility && React.createElement(
+          "div",
+          null,
+          React.createElement(
+            "p",
+            null,
+            " Here is some text! "
+          )
+        )
+      );
+    }
+  }]);
 
-visibilityToggle();
+  return VisibilityToggle;
+}(React.Component);
+
+ReactDOM.render(React.createElement(VisibilityToggle, null), document.getElementById("app"));
+
+// let visibility = false
+
+// let appRoot = document.getElementById("app")
+
+// const toggleText = () => {
+//   if (app.someText.length <= 0) {
+//     app.someText = "Here is some text!"
+//   } else {
+//     app.someText = ""
+//   }
+//   toggleButtonText()
+//   visibilityToggle()
+//   visibility = !visibility
+// }
+// const toggleButtonText = () => {
+//   if (app.buttonText === "Show Text") {
+//     app.buttonText = "Hide Text"
+//   } else {
+//     app.buttonText = "Show Text"
+//   }
+// }
+
+// const visibilityToggle = () => {
+//   var visibilityHTML = (
+//     <div>
+//       <h1>{app.title}</h1>
+
+//       <button onClick={toggleText}>{app.buttonText}
+//       {visibility ? "Hide Text" : "Show Text"}
+//       </button>
+//       {app.someText && <p>{app.someText}</p>}
+//       {visibility && (
+//         <div>
+//           <p> Here is some text! </p>
+//         </div>
+//       )}
+//     </div>
+//   )
+//   ReactDOM.render(visibilityHTML, appRoot)
+// }
+
+// visibilityToggle()
 
 // Another way:
 
--- src/playground/build-it-visibility.js
+++ src/playground/build-it-visibility.js
@@ -1,61 +1,85 @@
-// visibility toggle text
-// Button - when clicked shows something and changes to/from hide text to show text
-// Button shows text or removes text when clicked
-// text can be anything like hey this is visible
-
-// Look at app.js and counterExample.js
-
 // src/playground/build-it-visibility.js
 // babel src/playground/build-it-visibility.js --out-file=public/scripts/app.js --presets=env,react --watch
 
-var app = {
-  title: "Visibility Toggle",
-  someText: "",
-  buttonText: "Show Text"
+// VisibilityToggle -> render, constuctor, handleToggle
+
+// make visbility toggle and set to false
+
+class VisibilityToggle extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleVisibility = this.handleVisibility.bind(this)
+    this.state = {
+      visibility: false
+    }
+  }
+  handleVisibility() {
+    this.setState((prevState) => {
+      return {
+        visibility: !prevState.visibility,
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={this.handleVisibility}>{this.state.visibility ? "Hide Text" : "Show Text"}</button>
+        {this.state.visibility && (
+          <div>
+            <p> Here is some text! </p>
+          </div>
+        )}
+      </div>
+    )
+  }
 }
+
+ReactDOM.render(<VisibilityToggle />, document.getElementById("app"))
 
 // let visibility = false
 
-let appRoot = document.getElementById("app")
+// let appRoot = document.getElementById("app")
 
-const toggleText = () => {
-  if (app.someText.length <= 0) {
-    app.someText = "Here is some text!"
-  } else {
-    app.someText = ""
-  }
-  toggleButtonText()
-  visibilityToggle()
-  visibility = !visibility
-}
-const toggleButtonText = () => {
-  if (app.buttonText === "Show Text") {
-    app.buttonText = "Hide Text"
-  } else {
-    app.buttonText = "Show Text"
-  }
-}
+// const toggleText = () => {
+//   if (app.someText.length <= 0) {
+//     app.someText = "Here is some text!"
+//   } else {
+//     app.someText = ""
+//   }
+//   toggleButtonText()
+//   visibilityToggle()
+//   visibility = !visibility
+// }
+// const toggleButtonText = () => {
+//   if (app.buttonText === "Show Text") {
+//     app.buttonText = "Hide Text"
+//   } else {
+//     app.buttonText = "Show Text"
+//   }
+// }
 
-const visibilityToggle = () => {
-  var visibilityHTML = (
-    <div>
-      <h1>{app.title}</h1>
+// const visibilityToggle = () => {
+//   var visibilityHTML = (
+//     <div>
+//       <h1>{app.title}</h1>
 
-      <button onClick={toggleText}>{app.buttonText}
-      {visibility ? "Hide Text" : "Show Text"}
-      </button>
-      {app.someText && <p>{app.someText}</p>}
-      {visibility && (
-        <div>
-          <p> Here is some text! </p>
-        </div>
-      )}
-    </div>
-  )
-  ReactDOM.render(visibilityHTML, appRoot)
-}
+//       <button onClick={toggleText}>{app.buttonText}
+//       {visibility ? "Hide Text" : "Show Text"}
+//       </button>
+//       {app.someText && <p>{app.someText}</p>}
+//       {visibility && (
+//         <div>
+//           <p> Here is some text! </p>
+//         </div>
+//       )}
+//     </div>
+//   )
+//   ReactDOM.render(visibilityHTML, appRoot)
+// }
 
-visibilityToggle()
+// visibilityToggle()
 
 // Another way:
 
