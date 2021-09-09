4547d6b097c417654aa236d7d060b1695742a6eb
taylorod <taylorsunwantedemails@gmail.com>
2021-09-09 13:45:51 -0400

    add react stateless components

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -83,141 +83,124 @@ var IndecisionApp = function (_React$Component) {
   return IndecisionApp;
 }(React.Component);
 
-var Header = function (_React$Component2) {
-  _inherits(Header, _React$Component2);
+var Header = function Header(props) {
+  return React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "h1",
+      null,
+      props.title
+    ),
+    React.createElement(
+      "h2",
+      null,
+      props.subtitle
+    )
+  );
+};
+// class Header extends React.Component {
+//   render () {
+//     return (
+//       <div>
+//         <h1>{this.props.title}</h1>
+//         <h2>{this.props.subtitle}</h2>
+//       </div>
+//     )
+//   }
+// }
 
-  function Header() {
-    _classCallCheck(this, Header);
+var Action = function Action(props) {
+  return React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "button",
+      { onClick: props.handlePick, disabled: !props.hasOptions },
+      "What should I do?"
+    )
+  );
+};
+// class Action extends React.Component {
+//   render () {
+//     return (
+//       <div>
+//         <button
+//           onClick={this.props.handlePick}
+//           disabled={!this.props.hasOptions}
+//         >
+//           What should I do?
+//         </button>
+//       </div>
+//     )
+//   }
+// }
+var Options = function Options(props) {
+  return React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "button",
+      { onClick: props.handleDelateOptions },
+      " Remove All Options"
+    ),
+    React.createElement(
+      "p",
+      null,
+      " Render Options Here like the ones below"
+    ),
+    props.options.map(function (option) {
+      return React.createElement(Option, { key: option, optionText: option });
+    }),
+    React.createElement(Option, null)
+  );
+};
+// class Options extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.props.handleDelateOptions}> Remove All Options</button>
+//         <p> Render Options Here like the ones below</p>
+//         {this.props.options.map((option) => (
+//           <Option key={option} optionText={option} />
+//         ))}
+//         <Option />
+//       </div>
+//     )
+//   }
+// }
 
-    return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).apply(this, arguments));
-  }
+var Option = function Option(props) {
+  return React.createElement(
+    "div",
+    null,
+    "Option: ",
+    props.optionText
+  );
+};
+// class Option extends React.Component {
+//   render () {
+//     return (
+//       <div>
+//         Option: { this.props.optionText }
+//       </div>
+//     )
+//   }
+// }
 
-  _createClass(Header, [{
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "div",
-        null,
-        React.createElement(
-          "h1",
-          null,
-          this.props.title
-        ),
-        React.createElement(
-          "h2",
-          null,
-          this.props.subtitle
-        )
-      );
-    }
-  }]);
-
-  return Header;
-}(React.Component);
-
-var Action = function (_React$Component3) {
-  _inherits(Action, _React$Component3);
-
-  function Action() {
-    _classCallCheck(this, Action);
-
-    return _possibleConstructorReturn(this, (Action.__proto__ || Object.getPrototypeOf(Action)).apply(this, arguments));
-  }
-
-  _createClass(Action, [{
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "div",
-        null,
-        React.createElement(
-          "button",
-          {
-            onClick: this.props.handlePick,
-            disabled: !this.props.hasOptions
-          },
-          "What should I do?"
-        )
-      );
-    }
-  }]);
-
-  return Action;
-}(React.Component);
-
-var Options = function (_React$Component4) {
-  _inherits(Options, _React$Component4);
-
-  function Options() {
-    _classCallCheck(this, Options);
-
-    return _possibleConstructorReturn(this, (Options.__proto__ || Object.getPrototypeOf(Options)).apply(this, arguments));
-  }
-
-  _createClass(Options, [{
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "div",
-        null,
-        React.createElement(
-          "button",
-          { onClick: this.props.handleDelateOptions },
-          " Remove All Options"
-        ),
-        React.createElement(
-          "p",
-          null,
-          " Render Options Here like the ones below"
-        ),
-        this.props.options.map(function (option) {
-          return React.createElement(Option, { key: option, optionText: option });
-        }),
-        React.createElement(Option, null)
-      );
-    }
-  }]);
-
-  return Options;
-}(React.Component);
-
-var Option = function (_React$Component5) {
-  _inherits(Option, _React$Component5);
-
-  function Option() {
-    _classCallCheck(this, Option);
-
-    return _possibleConstructorReturn(this, (Option.__proto__ || Object.getPrototypeOf(Option)).apply(this, arguments));
-  }
-
-  _createClass(Option, [{
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "div",
-        null,
-        "Option: ",
-        this.props.optionText
-      );
-    }
-  }]);
-
-  return Option;
-}(React.Component);
-
-var AddOption = function (_React$Component6) {
-  _inherits(AddOption, _React$Component6);
+var AddOption = function (_React$Component2) {
+  _inherits(AddOption, _React$Component2);
 
   function AddOption(props) {
     _classCallCheck(this, AddOption);
 
-    var _this6 = _possibleConstructorReturn(this, (AddOption.__proto__ || Object.getPrototypeOf(AddOption)).call(this, props));
+    var _this2 = _possibleConstructorReturn(this, (AddOption.__proto__ || Object.getPrototypeOf(AddOption)).call(this, props));
 
-    _this6.handleAddOption = _this6.handleAddOption.bind(_this6);
-    _this6.state = {
+    _this2.handleAddOption = _this2.handleAddOption.bind(_this2);
+    _this2.state = {
       error: undefined
     };
-    return _this6;
+    return _this2;
   }
 
   _createClass(AddOption, [{
@@ -259,69 +242,13 @@ var AddOption = function (_React$Component6) {
   return AddOption;
 }(React.Component);
 
-ReactDOM.render(React.createElement(IndecisionApp, null), document.getElementById("app"));
-
-// let appRoot = document.getElementById("app")
-// var app = {
-//   title: "Indecision App",
-//   subtitle: "Put your life in the hands of a computer",
-//   options: [],
-// }
-
-// const onFormSubmit = (event) => {
-//   event.preventDefault();
-//   const option = event.target.elements.option.value
-//   if (option) {
-//     app.options.push(option)
-//     event.target.elements.option.value = ""
-//     renderTemplate()
-//   }
-// }
-
-// const resetOptions = () => {
-//   app.options = []
-//   renderTemplate()
-// }
-
-// const onMakeDecision = () => {
-//   const randomNumber = Math.floor(Math.random() * app.options.length)
-//   const option = app.options[randomNumber]
-//   alert(option)
-// }
-
-
-// const numbers = [55, 100, 1200]
-
-// const renderTemplate = () => {
-//   var template = (
+// const User = () => {
+//   return (
 //     <div>
-//       <h1>{app.title}</h1>
-//       {app.subtitle && <p>{app.subtitle}</p>}
-//       <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
-//       <p>{app.options.length}</p>
-
-//       <button disabled={app.options.length === 0} onClick={onMakeDecision}> What should I do?</button>
-//       <button onClick={resetOptions}>Reset Options</button>
-//       {
-//         numbers.map((number) => {
-//           return <p key={number}>Number: {number}</p>
-//         })
-//       }
-//       <ol>
-//       {
-//         app.options.map((item) => {
-//           return <li key={item}>{item}</li>
-//         })
-//       }
-
-//       </ol>
-//       <form onSubmit={onFormSubmit}>
-//         <input type="text" name="option" />
-//         <button>Add Option</button>
-//       </form>
+//     <p>Name: {props.name}</p>
+//     <p>Age: {props.age}</p>
 //     </div>
 //   )
-//   ReactDOM.render(template, appRoot)
 // }
 
-// renderTemplate()
+ReactDOM.render(React.createElement(IndecisionApp, null), document.getElementById("app"));
--- src/app.js
+++ src/app.js
@@ -55,57 +55,88 @@ class IndecisionApp extends React.Component {
     )
   }
 }
-class Header extends React.Component {
-  render () {
-    return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <h2>{this.props.subtitle}</h2>
-      </div>
-    )
-  }
-}
 
-class Action extends React.Component {
-  render () {
-    return (
-      <div>
-        <button
-          onClick={this.props.handlePick}
-          disabled={!this.props.hasOptions}
-        >
-          What should I do?
-        </button>
-      </div>
-    )
-  }
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h2>{props.subtitle}</h2>
+    </div>
+  )
 }
+// class Header extends React.Component {
+//   render () {
+//     return (
+//       <div>
+//         <h1>{this.props.title}</h1>
+//         <h2>{this.props.subtitle}</h2>
+//       </div>
+//     )
+//   }
+// }
 
-class Options extends React.Component {
-  render() {
-    return (
-      <div>
-        <button onClick={this.props.handleDelateOptions}> Remove All Options</button>
-        <p> Render Options Here like the ones below</p>
-        {this.props.options.map((option) => (
-          <Option key={option} optionText={option} />
-        ))}
-        <Option />
-      </div>
-    )
-  }
+const Action = (props) => {
+  return (
+    <div>
+      <button onClick={props.handlePick} disabled={!props.hasOptions}>
+        What should I do?
+      </button>
+    </div>
+  )
 }
-
-class Option extends React.Component {
-  render () {
-    return (
-      <div>
-        Option: { this.props.optionText }
-      </div>
-    )
-  }
+// class Action extends React.Component {
+//   render () {
+//     return (
+//       <div>
+//         <button
+//           onClick={this.props.handlePick}
+//           disabled={!this.props.hasOptions}
+//         >
+//           What should I do?
+//         </button>
+//       </div>
+//     )
+//   }
+// }
+const Options = (props) => {
+  return (
+    <div>
+      <button onClick={props.handleDelateOptions}> Remove All Options</button>
+      <p> Render Options Here like the ones below</p>
+      {props.options.map((option) => (
+        <Option key={option} optionText={option} />
+      ))}
+      <Option />
+    </div>
+  )
 }
+// class Options extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.props.handleDelateOptions}> Remove All Options</button>
+//         <p> Render Options Here like the ones below</p>
+//         {this.props.options.map((option) => (
+//           <Option key={option} optionText={option} />
+//         ))}
+//         <Option />
+//       </div>
+//     )
+//   }
+// }
 
+const Option = (props) => {
+      return <div>Option: {props.optionText}</div>
+}
+// class Option extends React.Component {
+//   render () {
+//     return (
+//       <div>
+//         Option: { this.props.optionText }
+//       </div>
+//     )
+//   }
+// }
 class AddOption extends React.Component {
   constructor (props) {
     super(props)
@@ -136,69 +167,13 @@ class AddOption extends React.Component {
   }
 }
 
-ReactDOM.render(<IndecisionApp />, document.getElementById("app"))
-
-// let appRoot = document.getElementById("app")
-// var app = {
-//   title: "Indecision App",
-//   subtitle: "Put your life in the hands of a computer",
-//   options: [],
-// }
-
-// const onFormSubmit = (event) => {
-//   event.preventDefault();
-//   const option = event.target.elements.option.value
-//   if (option) {
-//     app.options.push(option)
-//     event.target.elements.option.value = ""
-//     renderTemplate()
-//   }
-// }
-
-// const resetOptions = () => {
-//   app.options = []
-//   renderTemplate()
-// }
-
-// const onMakeDecision = () => {
-//   const randomNumber = Math.floor(Math.random() * app.options.length)
-//   const option = app.options[randomNumber]
-//   alert(option)
-// }
-
-
-// const numbers = [55, 100, 1200]
-
-// const renderTemplate = () => {
-//   var template = (
+// const User = () => {
+//   return (
 //     <div>
-//       <h1>{app.title}</h1>
-//       {app.subtitle && <p>{app.subtitle}</p>}
-//       <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
-//       <p>{app.options.length}</p>
-
-//       <button disabled={app.options.length === 0} onClick={onMakeDecision}> What should I do?</button>
-//       <button onClick={resetOptions}>Reset Options</button>
-//       {
-//         numbers.map((number) => {
-//           return <p key={number}>Number: {number}</p>
-//         })
-//       }
-//       <ol>
-//       {
-//         app.options.map((item) => {
-//           return <li key={item}>{item}</li>
-//         })
-//       }
-
-//       </ol>
-//       <form onSubmit={onFormSubmit}>
-//         <input type="text" name="option" />
-//         <button>Add Option</button>
-//       </form>
+//     <p>Name: {props.name}</p>
+//     <p>Age: {props.age}</p>
 //     </div>
 //   )
-//   ReactDOM.render(template, appRoot)
 // }
 
-// renderTemplate()
+ReactDOM.render(<IndecisionApp />, document.getElementById("app"))
